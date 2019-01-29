c09813a65dece2c80814ef57981dc4e4397b330b
kouhei ishiro <kouhei1469@yahoo.co.jp>
2019-01-29 03:47:47 +0000

    バリデーション・エラーメッセージ追加

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -23,6 +23,7 @@ class FeedsController < ApplicationController
 
   def confirm
     @feed = Feed.new(feed_params)
+    render :new if @feed.invalid?
   end
 
   def show
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,5 @@
 class Feed < ApplicationRecord
+  validates :title, length: { in: 1..30}
+  validates :content, length: { in: 1..140}
   # belongs_to :user
 end

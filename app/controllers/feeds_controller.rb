6d4d83f6b665a5f982086d75ccdc75df687d3694
kouhei ishiro <kouhei1469@yahoo.co.jp>
2019-01-29 02:48:55 +0000

    CRUD機能および投稿前の確認画面を作成

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,14 +1,24 @@
 class FeedsController < ApplicationController
+  before_action :set_feed, only: [:show, :edit, :update, :destroy]
   def index
+    @feeds = Feed.all
   end
 
   def create
+    @feed = Feed.new(feed_params)
+    if @feed.save
+      redirect_to feeds_path, notice: "Feedを投稿しました"
+    else
+      render "new"
+    end
   end
 
   def new
+    @feed = Feed.new
   end
 
   def confirm
+    @feed = Feed.new(feed_params)
   end
 
   def show
@@ -18,8 +28,25 @@ class FeedsController < ApplicationController
   end
 
   def update
+    if @feed.update(feed_parms)
+      redirect_to feeds_path, notice: "Feedを編集しました"
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @feed.destroy
+    redirect_to feeds_path, notice: "Feedを削除しました"
+  end
+
+  private
+
+  def feed_params
+    params.require(:feed).permit(:title, :content, :image)
+  end
+
+  def set_feed
+    @feed = Feed.find(params[:id])
   end
 end

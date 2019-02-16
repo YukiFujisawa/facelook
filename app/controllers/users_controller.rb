a5f35fe69c249081340fd39e6fac4999eab0c47b
YukiFujisawa <g031y148yuki@gmail.com>
2019-02-16 17:55:42 +0900

    Revert スペース

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-       redirect_to user_path(@user.id), notice: "ユーザーを作成しました"
+      redirect_to user_path(@user.id), notice: "ユーザーを作成しました"
     else
       render "new"
     end
